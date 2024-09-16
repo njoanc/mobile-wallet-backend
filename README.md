47fbde7197d6886b362e2d72f2b8f03955a82089
Jeanne d'Arc NYIRANWIZA <njoanc@gmail.com>
2024-09-16 14:34:20 +0200

    add():readme and .env.example files

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# mobile-wallet-backend
+# mobile-wallet-api
+
+A wallet system API developed with NestJS, postgresql and typeorm
+
+### This API assumes the following functionalities and constraints to enforce security and integrity
+
+- A security private key will be generated for users on sign up
+- Abilities for users to be able to fund (with either card or bank transfer) and withdraw money to their wallet using the [Flutterwave](https://flutterwave.com/us/) payment gateway.
+- users will be able to perform peer to peer transactions to any of their beneficiaries and withdraw from their wallet using the transaction pin.
+- during the peer to peer transaction, the sender and receiver (sender's beneficiary) will receive email notifications of the transaction.
+- users can only use that security key for recovering their transaction pin.
+- The security key has to be kept in an ultrasafe manner as there is no way for users to recover their account once they lose the key
+
+- Find the API documentation [here](http://localhost:3000/docs)
+
+### Getting Started: Install Pacakges
+
+```bash
+ npm install
+```
+
+or
+
+```bash
+ yarn
+```
+
+Then you can then finally start the development server with the command
+
+```bash
+ npm run start
+```
+
+or
+
+```bash
+ yarn start
+```
+
+### Dockerizing the development environment
+
+use the command below to build the docker image
+
+```bash
+docker-compose up --build
+```
+
+### Running the Dev Docker container
+
+To run the application, use the command below:
+
+```bash
+ docker-compose up
+```
+
+the server will be running on http://localhost:3000
+
+### Deployed to dockerhub
+
+- Docker repository link: https://hub.docker.com/r/jehanne123/wallet
--- tests/user/use.controller.spec.ts
+++ tests/user/use.controller.spec.ts
@@ -17,78 +17,76 @@ import { MailService } from '@sendgrid/mail';
 jest.setTimeout(60000);
 
 describe('UserController', () => {
-    let controller: UserController;
-    let service: UserService;
-    let configService: ConfigService;
-    let authService: AuthService;
-    let mailService: MailService;
+  let controller: UserController;
+  let service: UserService;
+  let configService: ConfigService;
+  let authService: AuthService;
+  let mailService: MailService;
 
-    beforeEach(async () => {
-        const module: TestingModule = await Test.createTestingModule({
-            imports: [
-                AppModule,
-                TypeOrmModule.forFeature([User, Email]),
-                AuthModule,
-                MailModule,
-            ],
-            controllers: [UserController],
-            providers: [
-                UserService,
-                {
-                    provide: JwtService,
-                    useFactory: () => ({
-                        sign: jest.fn(() => true),
-                    }),
-                },
-                ConfigService,
-                MailService,
-                AuthService,
-                User,
-            ],
-        }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        AppModule,
+        TypeOrmModule.forFeature([User, Email]),
+        AuthModule,
+        MailModule,
+      ],
+      controllers: [UserController],
+      providers: [
+        UserService,
+        {
+          provide: JwtService,
+          useFactory: () => ({
+            sign: jest.fn(() => true),
+          }),
+        },
+        ConfigService,
+        MailService,
+        AuthService,
+        User,
+      ],
+    }).compile();
 
-        controller = module.get<UserController>(UserController);
-        service = module.get<UserService>(UserService);
-        configService = module.get<ConfigService>(ConfigService);
-        authService = module.get<AuthService>(AuthService);
-        mailService = module.get<MailService>(MailService);
+    controller = module.get<UserController>(UserController);
+    service = module.get<UserService>(UserService);
+    configService = module.get<ConfigService>(ConfigService);
+    authService = module.get<AuthService>(AuthService);
+    mailService = module.get<MailService>(MailService);
+  });
+
+  describe('POST /user/auth/register', () => {
+    it('controller to sign up a user', async () => {
+      const user = new SignUpDto();
+      user.email = 'test101@yahoo.com';
+      user.pin = '123456';
+      user.firstName = 'test';
+      user.lastName = 'test';
+      user.phone = '08012345678';
+      user.deviceId = '123456789';
+      try {
+        const authSpySignUpService = jest.spyOn(authService, 'signup');
+        const controllerResult = await controller.register(user);
+        expect(authSpySignUpService).toBeCalledWith(user);
+        expect(controllerResult).toBeCalledWith(user);
+      } catch (error) {
+        expect(error).toBeInstanceOf(Error);
+      }
     });
+  });
 
-    describe('POST /user/auth/register', () => {
-        it('controller to sign up a user', async () => {
-            const user = new SignUpDto();
-            user.email = 'dwave101@yahoo.com';
-            user.pin = '123456';
-            user.firstName = 'dwave';
-            user.lastName = 'dwave';
-            user.phone = '08012345678';
-            user.deviceId = '123456789';
-            try {
-                const authSpySignUpService = jest.spyOn(authService, 'signup');
-                const controllerResult = await controller.register(user);
-                expect(authSpySignUpService).toBeCalledWith(user);
-                expect(controllerResult).toBeCalledWith(user);
-            } catch (error) {
-                expect(error).toBeInstanceOf(Error);
-            }
-        });
+  describe('POST /user/auth/login', () => {
+    it('controller to login a user', async () => {
+      const login = new LogInUserDto();
+      login.email = 'test101@yahoo.com';
+      login.pin = '123456';
+      try {
+        const authSpyLoginService = jest.spyOn(authService, 'login');
+        const controllerResult = await controller.login(login);
+        expect(authSpyLoginService).toBeCalledWith(login);
+        expect(controllerResult).toBeCalledWith(login);
+      } catch (error) {
+        expect(error).toBeInstanceOf(Error);
+      }
     });
-
-    describe('POST /user/auth/login', () => {
-        it('controller to login a user', async () => {
-            const login = new LogInUserDto();
-            login.email = 'dwave101@yahoo.com';
-            login.pin = '123456';
-            try {
-                const authSpyLoginService = jest.spyOn(authService, 'login');
-                const controllerResult = await controller.login(login);
-                expect(authSpyLoginService).toBeCalledWith(login);
-                expect(controllerResult).toBeCalledWith(login);
-            } catch (error) {
-                expect(error).toBeInstanceOf(Error);
-            }
-        });
-    });
-
-    
+  });
 });
